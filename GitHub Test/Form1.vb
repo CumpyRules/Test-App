1f05a5fb1cb62cac0262bf7d9ef0b9000abd3f47
CumpyRules <j.roesner@live.de>
2018-09-14 23:29:23 +0200

    Revert "Added a Hello World MessageBox"
    This reverts commit ddd33f040558915413f187188f6913118f7da237.

--- GitHub Test/Form1.vb
+++ GitHub Test/Form1.vb
@@ -1,8 +1,5 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Application.DoEvents()
-
-        MessageBox.Show(Me, "Hello World!", "Hello World!", vbOK, vbInformation)
-
     End Sub
 End Class
